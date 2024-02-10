17bf8443f0324973c382141b9c1d3c7dd3ae8dc6
abhistormwastaken <abhistorm409@gmail.com>
2024-02-10 19:29:56 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Hero from "./components/Hero"
+import Demo from "./components/Demo"
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <div className="gradient"></div>
+    </main>
+    
+  )
+}
+
+export default App
