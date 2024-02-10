24c75a3efa7ef1720df6a06500f736d0f8614e86
abhistormwastaken <abhistorm409@gmail.com>
2024-02-11 00:47:46 +0530

    Fix handleSubmit function and update input value

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -8,7 +8,7 @@ function Demo() {
   })
 
   const handleSubmit = async (e) => {
-    
+
   }
 
   return (
@@ -16,10 +16,14 @@ function Demo() {
       {/* Input */}
       <div className='flex flex-col w-full gap-2'>
         <form className='relative flex justify-center items-center'
-          onSubmit={handleSubmit}
-        >
+          onSubmit={handleSubmit}>
           <img src={linkIcon} alt="link_icon" className='absolute left-0 my-2 ml-3 w-5'/>
-          <input type="url" placeholder='Enter a URL' value="" onChange={() => {}} required className='url_input peer'/>
+          <input type="url" placeholder='Enter a URL' value={article.url} 
+              onChange={(e) => setArticle(
+                {...article, url: e.target.value}
+              )} 
+              required className='url_input peer'
+          />
           <button type='submit' className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700'>
             <img src={tick} alt="tick" className='w-4'/>
           </button>
