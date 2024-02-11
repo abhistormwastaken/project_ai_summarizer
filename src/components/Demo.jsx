c4b194d9603151522e43873b4d4938dc5b5b282f
abhistormwastaken <abhistorm409@gmail.com>
2024-02-11 21:53:42 +0530

    Added URL history + Final Results display

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -7,11 +7,12 @@ function Demo() {
     url: '',
     summary: '',
   })
-
   const [allArticles, setAllArticles] = useState([])
+  const [copied, setCopied] = useState(``)
 
-  const [getSummary, { error, isLoading }] = useLazyGetSummaryQuery()
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery()
 
+  // Load data from localStorage when website is refreshed [onMount]
   useEffect(()=>{
     const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'))
 
@@ -20,23 +21,37 @@ function Demo() {
     }
   }, [])
 
+
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+    
     const { data } = await getSummary({
-      url: article.url
+      articleUrl: article.url
     })
     
     if(data?.summary){
       const newArticle = {...article, summary: data.summary}
       const updatedAllArticles = [newArticle, ...allArticles]
-
+      
+      // update state and localStorage
       setArticle(newArticle)
       setAllArticles(updatedAllArticles)
-
       localStorage.setItem('articles', JSON.stringify(updatedAllArticles))
     }
   }
+  
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl)
+    navigator.clipboard.writeText(copyUrl)
+    setTimeout(() => setCopied(''), 3000)
+  }
+  
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      handleSubmit(e);
+    }
+  };
 
   return (
     <section className='mt-16 w-full max-w-xl'>
@@ -49,6 +64,7 @@ function Demo() {
               onChange={(e) => setArticle(
                 {...article, url: e.target.value}
               )} 
+              // onKeyDown={handleKeyDown}
               required className='url_input peer'
           />
           <button type='submit' className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700'>
@@ -57,9 +73,57 @@ function Demo() {
         </form>
 
         {/* Summarized URL History */}
+        <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+          {allArticles.reverse().map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className='link_card'
+            >
+              <div className='copy_btn' onClick={() => handleCopy(item.url)}>
+                <img 
+                  src={copied === item.url ? tick : copy} 
+                  alt={copied === item.url ? "tick_icon" : "copy_icon"} 
+                  className='w-[40%] h-[40%] object-contain'
+                />
+              </div>
+              <p className='flex-1 font-santoshi text-blue-700 font-medium text-sm truncate'>
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
 
       {/* Results */}
+      <div
+      className='my-10 flex max-w-full justify-center items-center '>
+        {isFetching ? (
+          <img src={loader} alt="loader" className='w-20 h-20 object-contain'/>
+        ) :
+          error ? (
+            <p className='font-inter font-bold text-black text-center'>
+              Well, that wasn't supposed to happen...
+              <br />
+              <span className='font-satoshi font-normal text-gray-700'>
+                {error?.data?.error}
+              </span>
+            </p>
+          ) : (
+            article.summary && (
+              <div className='flex flex-col gap-3'>
+                <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+                  Article <span className='orange_gradient'>Summary</span>
+                </h2>
+                <div className='summary_box text-justify'>
+                  <p className='font-inter font-medium text-sm text-gray-700'>
+                    {article.summary}
+                  </p>
+                </div>
+              </div>
+            )
+          )}
+      </div>
     </section>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,14 +7,14 @@ function Hero() {
         <nav className='flex justify-between items-center w-full pt-3 mb-10'>
             <img src={logo} alt="website_logo" className='w-28 object-contain'/>
             <button type='button'
-                onClick={()=>{window.open("https://github.com/abhistormwastaken")}}
+                onClick={()=>{window.open("https://github.com/abhistormwastaken/ai-article-summarizer")}}
                 className='black_btn'
             >Github</button>
         </nav>
 
         <h1 className='head_text'>
             Summarize Articles with <br className='max-md:hidden' />
-            <span className='orange_gradient'>OpenAI GPT-4</span>
+            <span className='blue_gradient'>OpenAI GPT-4</span>
         </h1>
         <h2 className='desc'>
             Simplify your reading with Summize, an open-source article summarizer
